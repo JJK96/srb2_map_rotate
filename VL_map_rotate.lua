ee8b6c00eb2aa59453d3801bd8ebe5194f6d3080
Jan-Jaap Korpershoek <jjkorpershoek96@gmail.com>
2020-04-09 19:29:28 +0200

    Make the rotation maps a parameter

--- VL_map_rotate.lua
+++ VL_map_rotate.lua
@@ -1,7 +1,6 @@
 local map_rotation_random = CV_RegisterVar({"map_rotation_random", "Off", CV_NETVAR, CV_OnOff})
 
-local _maps = { "R0", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R9", "RA", "RB", "RA", "RB", "RC", "RD", "RE", "RF", "RG", "RH", "RI", "RJ", "RK", "RL", "RM", "RN", "RO", "RP", "RQ", "RR", "RS", "RT", "RU", "RV", "RW", "RX", "LC" }
-
+local rotation_maps = ""
 local order = {}
 local maps = {}
 local num_maps = 0 --increased later
@@ -11,6 +10,10 @@ local char_to_num = function(char)
 end
 
 local to_map_number = function(extended_map_number)
+    local num = tonumber(extended_map_number)
+    if num != nil then
+        return num
+    end
     local x = extended_map_number:sub(1,1)
     local y = extended_map_number:sub(2,2)
     local p = char_to_num(x)
@@ -19,13 +22,6 @@ local to_map_number = function(extended_map_number)
         q = 10 + char_to_num(y)
     end
     return ((36*p + q) + 100)
-end
-
-for k,v in pairs(_maps) do
-    local mapnum = to_map_number(v)
-    maps[mapnum] = true
-    order[num_maps] = mapnum
-    num_maps = num_maps + 1
 end
 
 local get_index = function(mapnum)
@@ -44,12 +40,28 @@ local next_map = function()
         local index = get_index(gamemap)
         newindex = (index+1) % num_maps
     end
-    print(newindex)
     return order[newindex]
 end
 
 addHook("MapLoad", function(mapnum)
     if maps[mapnum] then
         G_SetCustomExitVars(next_map())
+    end
+end)
+
+COM_AddCommand("rotation_maps", function(player, args)
+    if args == nil then
+        local string = "Rotation maps: " .. rotation_maps
+        local usage = "rotation_maps <maps>:\nto set the maps that should be in the rotation.\ne.g. rotation_maps \"R0 R1 R2 RX\""
+        CONS_Printf(player, string)
+        CONS_Printf(player, usage)
+    else
+        rotation_maps = args
+        for map in string.gmatch(args, "%S+") do
+            local mapnum = to_map_number(map)
+            maps[mapnum] = true
+            order[num_maps] = mapnum
+            num_maps = num_maps + 1
+        end
     end
 end)
